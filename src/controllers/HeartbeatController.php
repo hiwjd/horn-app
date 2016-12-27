dd6ab90d037f2ec284140b9fddc1752b81cad8ad
hiwjd <self@hiwjd.com>
2016-12-28 00:20:28 +0800

    fix缺少的引用

--- src/controllers/HeartbeatController.php
+++ src/controllers/HeartbeatController.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 use Horn\Util;
 use Horn\Queue;
+use Horn\IdGen;
 
 class HeartbeatController {
     protected $ci;
